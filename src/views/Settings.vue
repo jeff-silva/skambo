62c10b14c8de86bfbf6504847b26fa6b55f619af
Jeferson Inácio <jeferson.i.silva@gmail.com>
2020-02-29 21:31:54 -0300

    Commit

---
+++ src/views/Settings.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Settings</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Settings',
+});
+</script>
+
+
+<style scoped>
+</style>
